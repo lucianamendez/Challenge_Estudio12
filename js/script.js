c3439f4994eda089bc1c0176fb2c37e528758651
Luciana Mendez <dg.lucianamendez@gmail.com>
2018-01-13 18:49:47 -0300

    ädd fade slider and products sections

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var array = [
+    './../img/Estudio12-Challenge_0516.jpg',
+    './../img/Estudio12-Challenge_0516.jpg',
+    './../img/Estudio12-Challenge_0516.jpg'],
+    position = 0;
+    imgDuration = 6000;
+
+function sliderfade() {
+    document.getElementById('slider').className += "fadeOut";
+    setTimeout(function() {
+        document.getElementById('slider').src = array[position];
+        document.getElementById('slider').className = "";
+    },1000);
+    position++;
+    if (position == array.length) { position = 0; }
+    setTimeout(sliderfade, imgDuration);
+}
+sliderfade();
